fc661667d7b9dcc926c81cb2b47c7bdb9f3fc19e
Levy-from-Odessa <savelik12@gmail.com>
2020-04-11 15:41:16 +0300

    fixed

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 	$('.items').on('change', function () {
 		items = convetToObject($('.items').val().split(","))
 	})
+
 	function convetToObject(arr) {
 		let object = {}
 		for (let i = 0; i < arr.length; i++) {
@@ -21,18 +22,36 @@ $(document).ready(function () {
 	for (let i = 0; i < binds.length; i++) {
 		allBind.push(binds[i].split(':'))
 	}
+
 	function filterArray(inputResult, reqValue, index) {
 		let result = []
 		for (let item of inputResult) {
-			if(reqValue[index] == '?'){
+			if (reqValue[index] == '?') {
 				for (let i = 0; i < inputResult.length; i++) {
 					result[i] = inputResult[i]
 				}
-			}
-			else if (reqValue[index] == parseInt(item[index])) {
+			} else if (reqValue[index] == parseInt(item[index])) {
 				result.push(item)
 			}
 		}
+		return (result)
+	}
+
+	function moreFilterArray(inputResult, reqValue, diraction) {
+		let result = []
+		console.log(reqValue)
+		if (diraction == 1) {
+			let moreFirst = filterArray(inputResult, reqValue, 0)
+			console.log(moreFirst)
+			let moreSecond = filterArray(moreFirst, reqValue, 1)
+			result = moreSecond
+		} else if (diraction == 0) {
+			let moreFirst = filterArray(inputResult, reqValue, 2)
+			console.log(reqValue)
+			let moreSecond = filterArray(moreFirst, reqValue, 1)
+			result = moreSecond
+		}
+
 		return (result)
 	}
 	$('button').on('click', function () {
@@ -53,11 +72,34 @@ $(document).ready(function () {
 		let first = filterArray(allBind, reqValue, 0)
 		let second = filterArray(first, reqValue, 1)
 		let third = filterArray(second, reqValue, 2) //!filter
+
 		let mainResult
+		let moreResult
+		let secondFilter
 		for (word of third) {
-			console.log(base[parseInt(word[0])]) //!translate
+
+			// console.log(base[parseInt(word[0])]) //!translate
 			mainResult = base[parseInt(word[0])] + "-- " + base[parseInt(word[1])] + "-- " + base[parseInt(word[2])]
+			let secondReqValue = []
 			$('.result').append('<p> ' + mainResult + '</p>')
+			if (reqValue[0] && reqValue[2] == '?') {
+				secondReqValue.push(word[2], word[1], '?')
+				secondFilter = moreFilterArray(allBind, secondReqValue, 1) //!filtred from val:-:-
+				if (secondFilter.length > 0) {
+					for (moreWord of secondFilter) {
+						moreResult = base[parseInt(word[0])] + "-- " + base[parseInt(moreWord[1])] + "-- " + base[parseInt(moreWord[2])]
+						$('.result').append('<p> -> ' + moreResult + '</p>')
+					}
+				}} else if (reqValue[2] && reqValue[0] == '?') {
+				secondReqValue.push('?', word[1], word[0])
+				secondFilter = moreFilterArray(allBind, secondReqValue, 0) //!filtred -:-:val
+				if (secondFilter.length > 0) {
+					for (moreWord of secondFilter) {
+						moreResult = base[parseInt(moreWord[0])] + "-- " + base[parseInt(moreWord[1])] + "-- " + base[parseInt(word[2])]
+						$('.result').append('<p> -> ' + moreResult + '</p>')
+					}
+				}
+			}
 		}
 	});
 
